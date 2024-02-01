0bd9cc52fc97b509b8b921e91fd59dc099173d9d
Eliashernandez971 <157150959+Eliashernandez971@users.noreply.github.com>
2024-01-31 23:04:21 -0600

    Update livestream.sh

--- livestream.sh
+++ livestream.sh
@@ -3,7 +3,7 @@
 YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"
 KEY="ywp8-sj4w-zw12-wuag-476e"
 
-VIDEO_SOURCE="https://linear-351.frequency.stream/mt/studio/351/hls/master/playlist.m3u8"
+VIDEO_SOURCE="https://ia601608.us.archive.org/8/items/c-4rs-hd-720p-699968/C4r%27s_HD__720p___699968.mp4"
 
 while true; do
     ffmpeg -re -i "$VIDEO_SOURCE" -i "$AUDIO_SOURCE" \
