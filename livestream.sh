04b5876725c35f917eacc857d6acdbf0fcc7813e
Eliashernandez971 <157150959+Eliashernandez971@users.noreply.github.com>
2024-01-31 22:52:20 -0600

    Update livestream.sh

--- livestream.sh
+++ livestream.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"
-KEY="tu-clave-de-transmision"
+KEY="ywp8-sj4w-zw12-wuag-476e"
 
 VIDEO_SOURCE="https://linear-351.frequency.stream/mt/studio/351/hls/master/playlist.m3u8"
 
